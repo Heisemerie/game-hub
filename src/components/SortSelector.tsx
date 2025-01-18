d233eb1ab0c5d0fc34be3ece0788c990a540881e
Heisemerie <ejike162@gmail.com>
2025-01-18 18:55:23 +0100

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -2,22 +2,37 @@ import { BsChevronDown } from "react-icons/bs";
 import { Button } from "./ui/button";
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "./ui/menu";
 
-const SortSelector = () => {
+interface Prop {
+  onSelelectSortOrder: (sortOrder: string) => void;
+  sortOrderValue: string;
+}
+
+const SortSelector = ({ onSelelectSortOrder, sortOrderValue }: Prop) => {
+  const sortOrders: { value: string; label: string }[] = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find((order) => order.value === sortOrderValue);
+
   return (
     <MenuRoot>
       <MenuTrigger asChild>
         <Button variant="subtle" size="md" fontSize={"md"}>
-          Order by: Relevance
+            Order by: {currentSortOrder?.label || "Relevance"}
           <BsChevronDown />
         </Button>
       </MenuTrigger>
       <MenuContent>
-        <MenuItem value={"Relevance"}>Relevance</MenuItem>
-        <MenuItem value={"Date Added"}>Date Added</MenuItem>
-        <MenuItem value={"Name"}>Name</MenuItem>
-        <MenuItem value={"Release Date"}>Release Date</MenuItem>
-        <MenuItem value={"Popularity"}>Popularity</MenuItem>
-        <MenuItem value={"Average Rating"}>Average Rating</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem value={order.value} key={order.value} onClick={() => onSelelectSortOrder(order.value)}>
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuContent>
     </MenuRoot>
   );
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
 //rawg api supports cropping for specific dimensions eg 600x400
 
 const getCroppedImageUrl = (url: string, width: number, height: number) => {
+    if (!url) return '';
     const target = `media/`;
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/' + width + '/' + height + '/' + url.slice(index);
