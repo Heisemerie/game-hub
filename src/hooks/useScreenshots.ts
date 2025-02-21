fe000e69f4177145da319903181a71153387ee4a
Heisemerie <ejike162@gmail.com>
2025-02-21 02:41:50 +0100

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,16 @@
+import { Screenshot } from "@/entities/Screenshot";
+import { APIclient } from "@/services/apiClient";
+import { useQuery } from "@tanstack/react-query";
+
+const useScreenshots = (gameId: number) => {
+  const screenshotService = new APIclient<Screenshot>(
+    `/games/${gameId}/screenshots`
+  );
+
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: screenshotService.getAll,
+  });
+};
+
+export default useScreenshots;
