935c2d94fac3e5aa15f3b9a33107f2ee0c3db0b7
Heisemerie <ejike162@gmail.com>
2025-02-22 11:20:02 +0100

    Refactor: move scroll-to-top functionality into Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,19 @@
 import NavBar from "@/components/NavBar";
-import ScrollToTop from "@/components/ScrollToTop";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.scrollTo({ top: 0 });
+      console.log("scroll to top");
+    }
+  }, [location]);
   return (
     <>
-      <ScrollToTop>
-        <NavBar />
-        <Outlet />
-      </ScrollToTop>
+      <NavBar />
+      <Outlet />
     </>
   );
 };
