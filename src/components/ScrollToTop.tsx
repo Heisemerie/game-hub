9fe4452e88dfdca09d4fb0a13ed1d7f835103eb2
Heisemerie <ejike162@gmail.com>
2025-02-22 10:11:15 +0100

    Fixed scroll issue

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useEffect } from "react";
+import { useLocation} from "react-router-dom";
+
+interface Props {
+  children: ReactNode;
+}
+
+const ScrollToTop = ({ children }: Props) => {
+  const location = useLocation();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.scrollTo({ top: 0, left: 0 });
+      console.log("scroll to top");
+    }
+  }, [location]);
+  return <>{children}</>;
+};
+
+export default ScrollToTop;
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,14 @@
 import NavBar from "@/components/NavBar";
+import ScrollToTop from "@/components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <ScrollToTop>
+        <NavBar />
+        <Outlet />
+      </ScrollToTop>
     </>
   );
 };
