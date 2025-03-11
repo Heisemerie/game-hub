87cdb21733bf87a4d94a351e104d7f254e646b46
Heisemerie <ejike162@gmail.com>
2025-03-11 17:44:50 +0100

    Fix: optimise with react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const ReactCompilerConfig = {
+  target: "18", // Set this to your React version ('17' | '18' | '19')
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler", { target: "18" }]],
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
       },
     }),
     tsconfigPaths(),
