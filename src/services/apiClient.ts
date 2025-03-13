131d0b41d4d8a98924988ab4b38b242dd33bca01
Heisemerie <ejike162@gmail.com>
2025-03-13 21:26:54 +0100

    Refactor: hide api keys

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
+const apiKey = import.meta.env.VITE_API_KEY
+
 export interface FetchResponse<D> {
   count: number;
   next: string | null;
@@ -9,7 +11,7 @@ export interface FetchResponse<D> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "75b6e2dad1df4e60a3029047ac5b9dd9",
+    key: apiKey,
   },
 });
 
